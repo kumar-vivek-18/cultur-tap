1f7bd1a661598615739b96e7642fa01007b8ad12
Vivek Kumar <vivek@culturtap.com>
2024-03-27 20:25:55 +0530

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='overflow-x-hidden'>
+    <div className=''>
       <Navbar />
 
       <Routes>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,10 +8,10 @@ const Navbar = () => {
     const [query, setQuery] = useState("");
 
     return (
-        <div className='w-full px-16 max-md:px-8'>
+        <div className='w-full max-sm:px-0 px-16 max-md:px-8'>
             <div className='flex w-full justify-between items-center '>
                 <div>
-                    <img src={logo} alt="culturTap" className='w-[316px] h-[100px]' loading='lazy' />
+                    <img src={logo} alt="culturTap" className='w-[316px] h-[100px] max-sm:scale-75' loading='lazy' />
                 </div>
                 <div className='flex w-1/3 h-[60px] items-center border border-gray-300 rounded-3xl px-8 text-[#2e2c43] text-[20px] shadow-sm max-lg:hidden'>
                     <LuSearch className='scale-125 color-[2e2c43] opacity-70' />
@@ -23,23 +23,23 @@ const Navbar = () => {
                         className='border-none w-[350px] focus:outline-none px-4'
                     />
                 </div>
-                <div className='flex gap-6'>
+                <div className='flex gap-6 max-sm:scale-75'>
                     <div className='w-[60px] h-[60px] rounded-full bg-[#d9d9d9]'></div>
                     <img src={settings} alt="settings" width="45px" height="45px" />
                 </div>
 
             </div>
-            <div className='flex gap-4 pt-[30px]'>
+            <div className='flex max-sm:px-4 overflow-x-scroll gap-4 pt-[30px] max-md:text-[14px] text-[16px]'>
                 <div className='flex items-center gap-2'>
-                    <Link to='/' className='text-[16px] font-semibold'>Trip Planning Call</Link>
+                    <Link to='/' className=' font-semibold'>Trip Planning Call</Link>
                     <p className='w-[24px] h-[24px] text-white flex justify-center items-center bg-[#faa02e] rounded-full'>0</p>
                 </div>
                 <div className='flex items-center gap-2'>
-                    <Link to='/localassistance' className='text-[16px] font-semibold'>Local Assistance</Link>
+                    <Link to='/localassistance' className=' font-semibold'>Local Assistance</Link>
                     <p className='w-[24px] h-[24px] text-white flex justify-center items-center bg-[#faa02e] rounded-full'>0</p>
                 </div>
                 <div className='flex items-center gap-2'>
-                    <Link to='/help' className='text-[16px] font-semibold'>Help</Link>
+                    <Link to='/help' className=' font-semibold'>Help</Link>
                     <p className='w-[24px] h-[24px] text-white flex justify-center items-center bg-[#faa02e] rounded-full'>0</p>
                 </div>
             </div>
